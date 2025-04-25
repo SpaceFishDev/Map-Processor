e0cb7e13c4b6ee6334eba4fa00305a4002856475
Liam Gatter <liamgatter@pop-os.localdomain>
2025-04-25 07:12:31 +0200

    everything

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using SkiaSharp;
+class Program
+{
+    static void Main(String[] Args)
+    {
+        if (Args.Length < 1)
+        {
+            Console.WriteLine("Please provide this format:\n[File Path]");
+            return;
+        }
+        using var input = File.OpenRead(Args[0]);
+        using var bitmap = SKBitmap.Decode(input);
+        Console.WriteLine("Starting Processing");
+        for(int x = 0; x < bitmap.Width; x++)
+        {
+            if(x % 100 == 0){
+                Console.WriteLine($"{(float)x/(float)bitmap.Width*100.0f}%");
+            }
+            for(int y = 0; y < bitmap.Height; y++)
+            {
+                var pixelColor = bitmap.GetPixel(x,y);
+                float avg = pixelColor.Red + pixelColor.Green;
+                avg /= 2;
+                avg = 255-avg;
+                var newPix = new SKColor((byte)avg,(byte)avg,(byte)avg);
+                bitmap.SetPixel(x,y,newPix);
+            }
+        }
+        var img = bitmap.Encode(SKEncodedImageFormat.Png, 100);
+        File.WriteAllBytes("processed-img.png", img.ToArray());
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Map Processor
+This is a processor for maps, this takes color elevation maps and turns them into black and white height maps.
